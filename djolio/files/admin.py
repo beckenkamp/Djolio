8fb917e1dcae827817b6824b40d256261c94a74d
Markus Beck <mvbeck@gmail.com>
2012-06-06 11:51:44 -0300

    Mostrando coluna project na lista de Files no admin

--- djolio/files/admin.py
+++ djolio/files/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from djolio.files.models import Files
 
-admin.site.register(Files)
+class FilesAdmin(admin.ModelAdmin):
+    list_display = ('name', 'project')
+
+admin.site.register(Files, FilesAdmin)
 
