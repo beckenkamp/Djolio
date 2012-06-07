cd61aeb7aeb0a2cdc00854be86ea90efa3d4d4b3
Markus Beck <mvbeck@gmail.com>
2012-06-07 20:07:23 -0300

    Criados os arquivos de exemplo do local_settings e do project_conf

---
+++ djolio/local_settings_example.py
@@ -0,0 +1,16 @@
+#Local settings for djolio project.
+
+import os
+ROOTDIR = os.path.realpath(os.path.dirname(__file__)) + '/'
+STATIC_HOST = ''
+STATIC_DIR = ''
+TEMPLATES_DIR = ROOTDIR + 'templates/'
+UPLOADEDFILES_HOST = STATIC_HOST + 'upload/'
+UPLOADEDFILES_DIR = STATIC_DIR + 'upload/'
+
+DB_ENGINE = ''
+DB_NAME = ''
+DB_USER = ''
+DB_PASSWORD = ''
+DB_HOST = ''
+DB_PORT = ''
---
+++ djolio/project_conf_example.py
@@ -0,0 +1,4 @@
+#Project specific configuration info
+
+PROJECT_NAME = 'My Project'
+USE_DARKSTRAP_CSS = True #Choose if you want to use darkstrap.css, this changes the standard bootstrap style to a dark one 
