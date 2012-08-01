434ff72d568e7500341c27ca952504a57d364dc0
Markus Beck <mvbeck@gmail.com>
2012-08-01 11:16:00 -0500

    Adicionando feed com os ultimos posts do blog na pagina inicial do portfolio

--- djolio/tags/templatetags/tags.py
+++ djolio/tags/templatetags/tags.py
@@ -9,19 +9,43 @@ def navactive(request, urls):
         return "active"
     return ""
 
-#from django.conf import settings # import the settings file
-#allows settings constant to be used on template
-#@register.tag
-#def value_from_settings(parser, token):
-#    try:
-#        # split_contents() knows not to split quoted strings.
-#        tag_name, var = token.split_contents()
-#    except ValueError:
-#        raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents.split()[0]
-#    return ValueFromSettings(var)
-#
-#class ValueFromSettings(template.Node):
-#    def __init__(self, var):
-#        self.arg = template.Variable(var)
-#    def render(self, context):        
-#        return settings.__getattr__(str(self.arg))
+
+#Para leitura de RSS no template
+import feedparser
+
+class RssParserNode(template.Node):
+    def __init__(self, var_name, url=None, url_var_name=None):
+        self.url = url
+        self.url_var_name = url_var_name
+        self.var_name = var_name
+
+    def render(self, context):
+        if self.url:
+            context[self.var_name] = feedparser.parse(self.url)
+        else:
+            try:
+                context[self.var_name] = feedparser.parse(context[self.url_var_name])
+            except KeyError:
+                raise template.TemplateSyntaxError, "the variable \"%s\" can't be found in the context" % self.url_var_name
+        return ''
+
+import re
+
+@register.tag(name="get_rss")
+def get_rss(parser, token):
+    # This version uses a regular expression to parse tag contents.
+    try:
+        # Splitting by None == splitting by spaces.
+        tag_name, arg = token.contents.split(None, 1)
+    except ValueError:
+        raise template.TemplateSyntaxError, "%r tag requires arguments" % token.contents.split()[0]
+    
+    m = re.search(r'(.*?) as (\w+)', arg)
+    if not m:
+        raise template.TemplateSyntaxError, "%r tag had invalid arguments" % tag_name
+    url, var_name = m.groups()
+    
+    if url[0] == url[-1] and url[0] in ('"', "'"):
+        return RssParserNode(var_name, url=url[1:-1])
+    else:
+        return RssParserNode(var_name, url_var_name=url)
